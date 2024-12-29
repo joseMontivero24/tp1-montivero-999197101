82b0d9a4f054b064dc1c8997bbbb59e7537443ad
Jose Montivero <chipimontivero2019@gmail.com>
2024-12-29 20:55:29 -0300

    hola

---
+++ main.js
@@ -0,0 +1,16 @@
+// // Seleccionamos los elementos necesarios
+// const btnHamburguesa = document.getElementById('btn-hamburguesa');
+// const btnCerrar = document.getElementById('btn-cerrar');
+// const menu = document.getElementById('menu');
+
+// console.log('hola')
+
+// // Mostrar el menú al hacer clic en la hamburguesa
+// btnHamburguesa.addEventListener('click', () => {
+//     menu.classList.add('visible');
+// });
+
+// // Ocultar el menú al hacer clic en el botón de cerrar
+// btnCerrar.addEventListener('click', () => {
+//     menu.classList.remove('visible');
+// });
